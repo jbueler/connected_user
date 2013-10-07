3ddb1c66d48bc188edca56e475a2a27be64ec79e
Jeremy Bueler <jbueler@gmail.com>
2013-10-07 11:50:43 -0700

    :punch: Adding session controller options stubbed out basic auth, and tested against current_user:add_provider :twitter

--- lib/generators/connected_user/install/install_generator.rb
+++ lib/generators/connected_user/install/install_generator.rb
@@ -22,6 +22,38 @@ module ConnectedUser
         template 'connection_model.rb.erb', "app/models/connection.rb"
         migration_template 'migrations/create_connections_table.rb.erb', 'db/migrate/create_connections_table'
         migration_template 'migrations/create_connected_user_model.rb.erb', 'db/migrate/create_connected_user_model'
+        
+        if ask("Would you like to create a Session Controller? [y/n]").downcase == 'y'
+          template 'session_controller.rb.erb', "app/controllers/session_controller.rb"
+          
+          # ADD CURRENT_USER HELPER
+          inject_into_file 'app/controllers/application_controller.rb', :after => "protect_from_forgery with: :exception" do
+          <<-eos
+
+  private
+    def current_user
+      @current_user ||= #{name}.find(session[:user_id]) if session[:user_id]
+    end
+    helper_method :current_user
+
+    def set_current_user (#{@underscored_user_name.downcase})
+      if user
+        @current_user = #{@underscored_user_name.downcase}
+        session[:#{@underscored_user_name.downcase}_id] = @current_user.id
+      else
+        raise UserLoginError
+      end
+    end
+          eos
+          end
+          route_file = "config/routes.rb"
+          inject_into_file route_file, "\n  get '/logout' => 'session#destroy', :as => :logout", :after => /Application.routes.draw do*/
+          inject_into_file route_file, "\n  post '/auth/:provider/callback' => 'session#create'", :after => /Application.routes.draw do*/
+          inject_into_file route_file, "\n  get '/auth/:provider/callback' => 'session#create'", :after => /Application.routes.draw do*/
+          inject_into_file route_file, "\n  get '/auth/:provider' => 'session#create', :as => :login", :after => /Application.routes.draw do*/
+          
+        end
+        
       end
       
     end
